b1db3c5651362126236f2c9cbec6abb411bb3287
cohen5790 <cohen5790@gmail.com>
2022-03-07 18:58:46 -0500

    MERN full-stack

---
+++ src/pages/App/App.jsx
@@ -0,0 +1,14 @@
+import { render } from '@testing-library/react';
+import { Component } from 'react'
+import './App.css';
+
+export default class App extends Component {
+  render() {
+    return (
+      <main className="App">
+        <h1>Rx-Tracker</h1>
+      </main>
+    );
+  }
+}
+
